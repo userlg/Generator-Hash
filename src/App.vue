3d2014d2d82bb56d5d91c352549cf2e9fc43f91a
userlg <guipeluis90@gmail.com>
2023-08-19 20:27:56 -0400

    First commit to this App

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+//import { ref } from 'vue';
+
+</script>
+<template>
+
+
+<h2 class="text-indigo-500 font-serif text-2xl font-semibold text-center my-10">Probando App</h2>
+
+</template>
